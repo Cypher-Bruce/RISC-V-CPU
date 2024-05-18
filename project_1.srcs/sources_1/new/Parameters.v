5be6168ee578a89910527166b5cc58d6a1c820cf
Cypher Bruce <53269096+Cypher-Bruce@users.noreply.github.com>
2024-05-19 00:02:07 +0800

    Delete project_1.srcs directory

--- project_1.srcs/sources_1/new/Parameters.v
+++
@@ -1,9 +0,0 @@
-///parameters.v
-
-// Instruction memory
-`define instruction_initial_address 32'h00000000
-
-// Data memory
-`define IO_device_initial_address 16'hFFC0 
-`define led_initial_address 16'hFFC0
-`define switch_initial_address 16'hFFC4
