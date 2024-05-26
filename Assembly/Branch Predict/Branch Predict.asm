3c133d384849dda26c95561e914d1c9d837a5191
Cypher Bruce <12212006@mail.sustech.edu.cn>
2024-05-26 21:17:38 +0800

    Update Branch Predict

--- Assembly/Branch Predict/Branch Predict.asm
+++ Assembly/Branch Predict/Branch Predict.asm
@@ -1,12 +1,15 @@
 .data
 .text
 
+li t1 0x00000080
+li a0, 0
+
 loop:
-li a0, 1
-add a0, a0, a0
-add a0, a0, a0
-add a0, a0, a0
-j loop
+addi a0, a0, 1
+add a1, a0, a0
+add a1, a1, a1
+add a1, a1, a1
+bne a1, t1, loop
 
 li a1, 1
 li a2, 1
