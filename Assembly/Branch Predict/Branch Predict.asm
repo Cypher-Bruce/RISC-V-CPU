4cfc122d8a42deeec0aa5732695f930b5394cfe1
Cypher Bruce <12212006@mail.sustech.edu.cn>
2024-05-26 21:11:35 +0800

    Add Branch Predict

---
+++ Assembly/Branch Predict/Branch Predict.asm
@@ -0,0 +1,12 @@
+.data
+.text
+
+loop:
+li a0, 1
+add a0, a0, a0
+add a0, a0, a0
+add a0, a0, a0
+j loop
+
+li a1, 1
+li a2, 1
---
+++ Pipeline_CPU.srcs/sources_1/new/Branch_Predict_tb.v
@@ -0,0 +1,36 @@
+`timescale 1ns / 1ps
+
+module Branch_Predict_tb();
+
+    reg             raw_clk;
+    reg             raw_rst;
+    reg  [23:0]     switch;
+    reg  [4:0]      button;
+    wire [23:0]     led;
+    wire [7:0]      tube_select_onehot;
+    wire [7:0]      tube_shape;
+    reg             rx;
+    wire            tx;
+
+    Top Top_Instance(
+        .raw_clk(raw_clk),
+        .raw_rst(raw_rst),
+        .switch(switch),
+        .button(button),
+        .led(led),
+        .tube_select_onehot(tube_select_onehot),
+        .tube_shape(tube_shape),
+        .rx(rx),
+        .tx(tx)
+    );
+
+    initial begin
+        raw_clk = 0;
+        raw_rst = 0;
+        switch = 0;
+        button = 0;
+        rx = 0;
+        forever #5 raw_clk = ~raw_clk;
+    end
+
+endmodule
